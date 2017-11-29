d1227a77a1ce6dd5e06ad12f812b84d98ff54c6b
YuushiOnozawa <31531668+YuushiOnozawa@users.noreply.github.com>
2017-11-29 15:11:02 +0900

    edit server.js

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const serveStatic = require('serve-static');
 
 app.set('port', process.env.PORT || 3000);
 app.use(serveStatic(`${__dirname}/dest`));
+
+app.listen(app.get('port'), () => {
+  console.log(`listening:${app.get('port')}`);
+});
